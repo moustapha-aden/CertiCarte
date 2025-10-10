3179063bf3c7b4191159625d1e20b8fc6b115d22
MOUSTAPHA ADEN <168269942+moustapha-aden@users.noreply.github.com>
2025-10-10 18:12:15 +0300

    refactor: Update id card template to display place of birth instead of address

--- resources/views/reports/id-card.blade.php
+++ resources/views/reports/id-card.blade.php
@@ -515,7 +515,7 @@
                             <div class="info-line info-address">
                                 <div class="info-line-content">
                                     <div class="info-label">Adresse</div>
-                                    <div class="info-value">{{ $student->address ?? 'Non renseignée' }}</div>
+                                    <div class="info-value">{{ $student->place_of_birth ?? 'Non renseignée' }}</div>
                                 </div>
                             </div>
                         </div>
